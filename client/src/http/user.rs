98e20e0705d5b5b42aa7c5c357d1964e69f20845
ToastXC <100072983+toastxc@users.noreply.github.com>
2023-03-22 10:45:40 +0800

    created method bp

--- client/src/http/user.rs
+++ client/src/http/user.rs
@@ -1,4 +1,26 @@
+use crate::data::register::DataUserRegister;
 
-pub struct User {}
+use super::common::Error;
 
-impl User {}
+#[derive(Debug, Clone)]
+pub struct User {
+    pub userid: String,
+}
+
+impl User {
+    pub async fn register(data: DataUserRegister) -> Result<Self, Error> {
+        let curl = reqwest::Client::new()
+            .post("http://localhost:8000/register")
+            .body(serde_json::to_string(&data).unwrap())
+            .send()
+            .await
+            .unwrap();
+
+        let status = curl.status();
+        match (status.is_success(), curl.text().await) {
+            (false, _) => Err(Error::Http(status)),
+            (true, Err(a)) => Err(Error::Json(a)),
+            (true, Ok(a)) => Ok(Self { userid: a }),
+        }
+    }
+}
